c8c88bc7d1ce6f9068850b8181989b6841f38416
a-laarabi <contact.laarabi@gmail.com>
2025-12-06 16:04:04 +0100

    Update cron && remove some numbers && update log.json

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ ensureLog();
 
 // Schedule: run every day at 18:00 (6 PM). Change to "0 9 * * *" for 9AM.
 // cron.schedule("* * * * *", async () => {
-cron.schedule("0 13 * * *", async () => {
+cron.schedule("30 15 * * *", async () => {
   console.log(`[${new Date().toISOString()}] Starting daily message batch...`);
 
   const data = loadJSON(dataFile, []);
